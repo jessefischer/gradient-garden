c2c7c00353c5357b05a1b092223f9adb36cbbcc4
Jesse Fischer <59373483+jessefischer@users.noreply.github.com>
2024-10-27 20:31:25 -0400

    Added comments

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,22 @@ import styles from "./App.module.css";
 import { SeedlingBubble } from "./SeedlingBubble";
 
 export const App = () => {
-  const mockBubbles = [
+  // This is temporary. Eventually we will be retrieving each seedling from the database.
+  const mockSeedlings = [
     { x: 150, y: 350, title: "Yellow Submarine", size: 250 },
     { x: 200, y: 650, title: "Space is the Place", size: 400 },
     { x: 550, y: 400, title: "Timelapse", size: 300 },
   ];
   return (
+    // React uses a language called JSX which is a fancy way of inserting HTML-like language into the middle of
+    // JavaScript code. It compiles down into something similar to document.createElement('div').
     <div className={styles.app}>
       <h1>Digital Gradient Garden</h1>
-      {mockBubbles.map(({ x, y, title, size }, i) => {
+      {/* The .map function takes an array in one format and maps each element onto a different format.
+          In this case, we take elements that are simple objects, and transform each one into a JSX element. */}
+      {mockSeedlings.map(({ x, y, title, size }, i) => {
+        // We have to define a unique key for each element in the resulting array in order for React to keep
+        // track of them properly
         return <SeedlingBubble key={i} title={title} x={x} y={y} size={size} />;
       })}
     </div>
--- src/components/SeedlingBubble.js
+++ src/components/SeedlingBubble.js
@@ -1,16 +1,22 @@
 import styles from "./SeedlingBubble.module.css";
 
+// This is one bubble defining a seedling on the main app window.
+// The props are title, x and y coordinates (of the center) and size (diameter)
 export const SeedlingBubble = ({ title, x, y, size }) => {
   return (
     <div
+      // React uses "className" instead of the normal "class" because "class" is already a reserved keyword
+      // in JavaScript with a different meaning.
       className={styles.seedlingBubble}
       style={{
-        top: y - size / 2,
-        left: x - size / 2,
+        top: y - size / 2, // We do the extra calculation because CSS is expecting top-left corner
+        left: x - size / 2, // instead of center for <div> elements
         height: size,
         width: size,
       }}
     >
+      {/* We use the curly braces to inject the props into the JSX output, so that
+          when any of the props change value, the HTML will update or "react" automatically */}
       {title}
     </div>
   );
