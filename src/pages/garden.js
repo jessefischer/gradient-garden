254fc76a7f3f98aee0c94de7b8e2624f2d8a7b81
matt <mattbowedesign@gmail.com>
2024-11-02 20:18:13 -0700

    added helper and instructions to garden

--- src/pages/garden.js
+++ src/pages/garden.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { App } from "@/components/App";
+import { HelperApp } from "@/components/HelperApp";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <App />
+      <HelperApp />
     </>
   );
 }
