c013d483a9e4cc37f6975fb40091b5fbb71a7c73
matt <mattbowedesign@gmail.com>
2024-11-05 18:39:28 -0800

    Added 300vh and 300vw to the canvas size

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,14 +1,12 @@
 import React from "react";
 import styles from "./Info.module.css";
+
 const Info = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return (
     <div className={styles.infoOverlay} onClick={onClose}>
       <div className={styles.infoContent} onClick={(e) => e.stopPropagation()}>
-        {/* <button className={styles.closeButton} onClick={onClose}>
-          &times;
-        </button> */}
         <h1>How to use Pollinator</h1>
         <div className={styles.infoIllustrations}>
           <div>
